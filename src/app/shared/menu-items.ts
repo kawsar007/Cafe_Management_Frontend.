21eb308aa91cbe6874ac1e4971c26a0d95649041
kawsar007 <imkawsar007@gmail.com>
2022-09-09 15:22:59 +0600

    Complete Category Section

--- src/app/shared/menu-items.ts
+++ src/app/shared/menu-items.ts
@@ -9,7 +9,7 @@ export interface Menu {
 
 const MENUITEMS = [
     {state: 'dashboard',name:'Dashboard',icon:'dashboard',role:''},
-    // {state: 'product',name:'Product',icon:'product',role:''},
+    {state: 'category',name:'Manage Category',icon:'category',role:'admin'},
     // {state: 'category',name:'Category',icon: 'category',role:''},
     // {state: 'bill',name:'Bill',icon: 'bill',role:''},
 ];
