83d0c1040f6479e17d067c6ab5847d2a8a19d2c6
kawsar007 <imkawsar007@gmail.com>
2022-09-19 10:01:01 +0600

    Manage Order Section Completed

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,34 +8,42 @@ import { environment } from 'src/environments/environment';
 export class ProductService {
   url = environment.apiUrl;
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  addProduct(data:any){
-     return this.httpClient.post(this.url+"/product/add/",data,{
-      headers: new HttpHeaders().set('Content-Type',"application/json")
-     })
-  }
-
-  updateProduct(data:any) {
-    return this.httpClient.patch(this.url+"/product/update/",data,{
-      headers: new HttpHeaders().set('Content-Type',"application/json")
-    })
-  }
-
-  getProducts(){
-    return this.httpClient.get(this.url+"/product/get/");
-  }
-
-  updateStatus(data:any){
-    return this.httpClient.patch(this.url+"/product/updateStatus/",data, {
-      headers: new HttpHeaders().set('Content-Type',"application/json")
-    })
-  }
-
-  deleteProduct(id:any){
-     return this.httpClient.delete(this.url+"/product/delete/"+id,{
+  addProduct(data: any) {
+    return this.httpClient.post(this.url + "/product/add/", data, {
       headers: new HttpHeaders().set('Content-Type', "application/json")
-     })
+    })
+  }
+
+  updateProduct(data: any) {
+    return this.httpClient.patch(this.url + "/product/update/", data, {
+      headers: new HttpHeaders().set('Content-Type', "application/json")
+    })
+  }
+
+  getProducts() {
+    return this.httpClient.get(this.url + "/product/get/");
+  }
+
+  updateStatus(data: any) {
+    return this.httpClient.patch(this.url + "/product/updateStatus/", data, {
+      headers: new HttpHeaders().set('Content-Type', "application/json")
+    })
+  }
+
+  deleteProduct(id: any) {
+    return this.httpClient.delete(this.url + "/product/delete/" + id, {
+      headers: new HttpHeaders().set('Content-Type', "application/json")
+    })
+  }
+
+  getProductsByCategory(id: any) {
+    return this.httpClient.get(this.url + "/product/getByCategory/" + id);
+  }
+
+  getById(id: any) {
+    return this.httpClient.get(this.url + "/product/getById/" + id);
   }
 
 }
--- src/app/shared/menu-items.ts
+++ src/app/shared/menu-items.ts
@@ -11,7 +11,7 @@ const MENUITEMS = [
     {state: 'dashboard',name:'Dashboard',icon:'dashboard',role:''},
     {state: 'category',name:'Manage Category',icon:'category',role:'admin'},
     {state: 'product',name:'Manage Product',icon: 'inventory_2',role:'admin'},
-    // {state: 'bill',name:'Bill',icon: 'bill',role:''},
+    {state: 'order',name:'Manage Order',icon: 'list_alt',role:''},
 ];
 
 @Injectable()
